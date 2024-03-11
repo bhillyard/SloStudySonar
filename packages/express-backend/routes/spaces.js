d62358a8780be298c7a8ef37dd286d36137e1536
cjduhamel <speed@duhamelandassociates.com>
2024-03-11 13:25:45 -0700

    reviews complete, ready to be used

--- packages/express-backend/routes/spaces.js
+++ packages/express-backend/routes/spaces.js
@@ -2,6 +2,7 @@ import express from "express";
 import spaces_methods from "../databaseServices/studySpaceServices.js";
 import middleware from "./middleware.js";
 import users_methods from "../databaseServices/userServices.js";
+import reviews_methods from "../databaseServices/reviewServices.js";
 
 const router = express.Router();
 router.use(express.json());
@@ -17,6 +18,26 @@ router.get("/", (req, res) => {
     }).catch((error) => {
       res.status(404).send("Spaces not found");
     });
+});
+
+//get all reviews
+router.get("/reviews", (req, res) => {
+  const author = req.query.author;
+
+  if(author != null){
+    reviews_methods.findReviewByAuthor(author).then((result) => {
+      res.status(200).send(result);
+    }).catch((error) => {
+      res.status(404).send("Reviews not found");
+    });
+  }else{
+    reviews_methods.getAllReviews().then((result) => {
+      res.status(200).send(result);
+    }).catch((error) => {
+      res.status(404).send("Reviews not found");
+    });
+  
+  }
 });
 
 // get a space by ID
@@ -44,6 +65,8 @@ router.post("/", middleware.authenticateUser, (req, res) => {
   
 });
 
+
+
 // delete a space by ID
 router.delete("/:id", (req, res) => {
   spaces_methods.deleteStudySpace(req.params.id).then((result) => {
@@ -53,6 +76,67 @@ router.delete("/:id", (req, res) => {
   });
 
 });
+
+
+router.get("/:id/reviews", (req, res) => {
+  const author = req.query.author;
+
+  if(author != null){
+    reviews_methods.findReviewByAuthor(author).then((result) => {
+      res.status(200).send(result);
+    }).catch((error) => {
+      res.status(404).send("Reviews not found");
+    });
+  }else{
+    reviews_methods.findReviewBySpace(req.params.id).then((result) => {
+      res.status(200).send(result);
+    }).catch((error) => {
+      res.status(404).send("Reviews not found");
+    });
+  }
+});
+
+router.post("/:id/reviews", middleware.authenticateUser, (req, res) => {
+  const user = req.userRef;
+  if(user == null){
+    res.status(403).send("User not authenticated, please sign in.");
+  }else{
+    users_methods.findUserById(user.id).then((result) => {
+      req.body.author = user.id;
+      req.body.space = req.params.id;
+      reviews_methods.addReview(req.body).then((result) => {
+        res.status(201).send(result);
+      }).catch((error) => {
+        res.status(400).send("Review not added");
+      });
+    }).catch((error) => {
+      res.status(404).send("User not found");
+    });
+  }
+});
+
+router.delete("/:id/reviews/:reviewId", middleware.authenticateUser, (req, res) => {
+  const user = req.userRef;
+  if(user == null){
+    res.status(403).send("User not authenticated, please sign in.");
+  }else{
+    reviews_methods.findReviewById(req.params.reviewId).then((result) => {
+      if(result.author == user.id){
+        reviews_methods.deleteReview(req.params.reviewId).then((result) => {
+          res.status(200).send(result);
+        }).catch((error) => {
+          res.status(404).send("Review not found");
+        });
+      }else{
+        res.status(403).send("User not authorized to delete this review");
+      }
+    }).catch((error) => {
+      res.status(404).send("Review not found");
+    });
+  }
+});
+
+
 
 
 export default router;
