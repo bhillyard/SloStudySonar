827c7c5707d5544319c35d84468920a6a25dae01
cheungkelly <cheungsinyikelly@gmail.com>
2024-03-12 13:33:01 -0700

    dashboard to global css

--- packages/react-frontend/src/pages/YourSessions.js
+++ packages/react-frontend/src/pages/YourSessions.js
@@ -5,51 +5,26 @@ import Navbar from '../Navigation/Navbar';
 
 function YourSessions() {
     return (
-        <div className="study-sessions-container">
+        <div className="study-sessions-container"> {/* This class should make the div use the full page */}
+        
         <Navbar />
-            <header className="App-header">
-            <h1>SLO Study Sonar</h1>
-            </header>  
+      <header className="App-header">
+        <h1>SLO Study Sonar</h1>
+      </header>
 
-        <div className="back-arrow-container">    
+      <div className="back-arrow-container">    
         <BackArrowButton onClick={() => { /* Add your click handler logic here */ }} /></div>
-       
-
-            <h1 className="sessions-title">Your Study Sessions</h1>
-            <table className="sessions-table">
-                <thead>
-                    <tr>
-                        <th>Location</th>
-                        <th>Time</th>
-                        <th>Info</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>SloDoCo</td>
-                        <td>10:00 AM - 12:00 PM</td>
-                        <td>Math Study Group</td>
-                    </tr>
-                    <tr>
-                        <td>Sierra Madre Community Center</td>
-                        <td>2:00 PM - 4:00 PM</td>
-                        <td>Physics Revision</td>
-                    </tr>
-                    <tr>
-                        <td>Baker</td>
-                        <td>6:00 PM - 8:00 PM</td>
-                        <td>Chemistry Homework</td>
-                    </tr>
-                    {/* Add more rows as needed */}
-                </tbody>
-            </table>
-
-            <footer>
-             <div class="container">
-                <p>&copy; 2024 SLO Study Sonar. All Rights Reserved.</p>
-            </div>
+    
+    <main className="sessions-content"></main>
+    <h2 className="sessions-title">Your Study Sessions</h2> {/* Use h2 if h1 is used for the main page title */}
+    <table className="sessions-table">
+        {/* table content */}
+    </table>
+    
+    <footer className="app-footer">
+            <p>&copy; 2024 SLO Study Sonar. All Rights Reserved.</p>
         </footer>
-        </div>
+    </div>    
     );
 }
 
